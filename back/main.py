401adc820f65e08091032dea9d244def7a4d62d8
TheoSourget <sourget.theo@live.fr>
2023-03-27 18:17:13 +0200

    OpenAlex request api route

---
+++ back/main.py
@@ -0,0 +1,70 @@
+from fastapi import FastAPI,HTTPException
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
+import requests
+
+app = FastAPI()
+
+
+
+def doi_to_OpenAlexId(doi):
+    base_url = f"https://api.openalex.org/works/doi:{doi}"
+    r = requests.get(base_url)
+    if r.status_code == 200:
+        r_json = r.json()
+        return r_json["id"]
+    else:
+        return None
+
+@app.get("/get_citations/openalex/")
+def request_OpenAlex(doi:str):
+
+    openalex_id = doi_to_OpenAlexId(doi)
+    print(openalex_id)
+    if not openalex_id:
+        raise HTTPException(status_code=500, detail="DOI couldn't be convert to OpenAlexID verify input")
+
+    base_url = "https://api.openalex.org/works"
+    ret = {}
+    
+    page_number = 1
+    paper_id = 1
+    next_page = True
+    
+    while  next_page:
+        query_param = {
+            "filter":f"cites:{openalex_id}",
+            "page":page_number
+        }
+        request = requests.get(base_url,params=query_param)
+        print(request.url)
+        if request.status_code == 200:
+            print()
+            request_json = request.json()
+            for res in request_json["results"]:
+                title = res["title"]
+                if title:
+                    title = title.replace(",","")
+                    title = title.replace("\n","")
+                
+                doi = res["doi"]
+                
+                year = res["publication_year"]
+                
+                ret[paper_id] = {}
+                ret[paper_id]["Title"]=title
+                ret[paper_id]["DOI"]=doi
+                ret[paper_id]["Year"]=year
+
+                paper_id += 1
+            
+            if not request_json["results"]:
+                next_page = False
+            else:
+                page_number += 1
+        else:
+            error_message = f"Error {request.status_code}: Search failed, please verify your input"
+            raise HTTPException(status_code=request.status_code, detail=error_message)
+    
+    json_response = jsonable_encoder(ret)
+    return JSONResponse(content=json_response)
