0aa92dee63b1342c16b320702c6b49afcd28031e
TheoSourget <sourget.theo@live.fr>
2023-03-29 19:39:38 +0200

    API route to search for keywords in abstract

--- back/main.py
+++ back/main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI,HTTPException
+from fastapi import FastAPI,HTTPException,Body
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 import requests
 from doi2bib.crossref import get_json
+import numpy as  np
+import time
 
 app = FastAPI()
 
@@ -163,3 +165,85 @@ def request_poci(doi:str):
     
     json_response = jsonable_encoder(ret)
     return JSONResponse(content=json_response)
+
+
+
+@app.get("/search_keywords/")
+def search_keywords(params: dict = Body(...)):
+    concepts = params["concepts"]
+    year = params["year"]
+    keywords = params["keywords"]
+    timeout = params["timeout"]
+    query_url = "https://api.openalex.org/works"
+    filter_str = ""
+    for c in concepts:
+        filter_str += f"concepts.id:{c},"
+    filter_str += f"publication_year:{year},has_abstract:true"
+    next_page = True
+
+    page_number = 1
+    ret = {}
+    id_paper = 1
+
+    time_begin = time.time()
+    while next_page:
+        query_param = {
+            'page':page_number,
+            'filter':filter_str
+        }
+        r = requests.get(query_url,params=query_param)
+        if r.status_code == 200:
+            r_json = r.json()
+            #If "results" field is empty we have reached the end for the current year
+            if not r_json["results"]:
+                next_page = False
+            else:
+                # For each paper in the current page
+                for paper in r_json["results"]:
+                    # Maximum size of the abstract, if the paper abstarct is longer it will be truncated
+                    abstract = np.full(2500,"",dtype=object)
+                    # The "abstract_inverted_index" field is a dictionnary with word as key and locations of this word in the abstract
+                    # So we fill the abstarct variable above at the index of the word to reconstruct the abstract
+                    for w in paper["abstract_inverted_index"]:
+                        for indices in paper["abstract_inverted_index"][w]:
+                            if indices < 2500:
+                                abstract[indices] = ''.join(filter(str.isalnum, w)).lower()
+                    # Remove empty location mostly due to a shorter abstract 
+                    abstract = abstract[abstract != ""]
+                    #Convert array to string
+                    str_abstract = ' '.join(abstract)
+
+                    #Get title and remove "," and "\n" that will create problem in the result csv 
+                    title = paper["title"]
+                    if title:
+                        title = title.replace(",","")
+                        title = title.replace("\n","")
+
+                    # Search for keyword in the abstract
+                    if keywords:
+                        for k in keywords:
+                            if k in str_abstract:
+                                ret[id_paper] = {}
+                                ret[id_paper]["Title"] = title
+                                ret[id_paper]["DOI"] = paper["doi"]
+                                ret[id_paper]["Year"] = paper["publication_year"]
+                                ret[id_paper]["Abstract"] = str_abstract
+                                id_paper += 1
+                                break
+                    else:
+                        ret[id_paper] = {}
+                        ret[id_paper]["Title"] = title
+                        ret[id_paper]["DOI"] = paper["doi"]
+                        ret[id_paper]["Year"] = paper["publication_year"]
+                        ret[id_paper]["Abstract"] = str_abstract
+                        id_paper += 1
+
+                    if time.time() - time_begin > timeout:
+                        next_page = False
+                        break
+                page_number += 1
+        else:
+            next_page=False
+
+    json_response = jsonable_encoder(ret)
+    return json_response
